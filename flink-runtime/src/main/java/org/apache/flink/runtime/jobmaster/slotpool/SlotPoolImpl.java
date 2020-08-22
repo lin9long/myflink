apache/flink

[FLINK-17019][runtime] Enable DualKeyLinkedMap for querying primary/secondary key with a given secondary/primary key

flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/SlotPoolImpl.java
@@ -356,7 +356,7 @@ public class SlotPoolImpl implements SlotPool {
 	}
 
 	private void slotRequestToResourceManagerFailed(SlotRequestId slotRequestID, Throwable failure) {
-		final PendingRequest request = pendingRequests.getKeyA(slotRequestID);
+		final PendingRequest request = pendingRequests.getByKeyA(slotRequestID);
 		if (request != null) {
 			if (isBatchRequestAndFailureCanBeIgnored(request, failure)) {
 				log.debug("Ignoring failed request to the resource manager for a batch slot request.");
@@ -694,7 +694,7 @@ public class SlotPoolImpl implements SlotPool {
 
 		componentMainThreadExecutor.assertRunningInMainThread();
 
-		final PendingRequest pendingRequest = pendingRequests.getKeyB(allocationID);
+		final PendingRequest pendingRequest = pendingRequests.getByKeyB(allocationID);
 		if (pendingRequest != null) {
 			if (isBatchRequestAndFailureCanBeIgnored(pendingRequest, cause)) {
 				log.debug("Ignoring allocation failure for batch slot request {}.", pendingRequest.getSlotRequestId());
@@ -1050,11 +1050,11 @@ public class SlotPoolImpl implements SlotPool {
 		 * @return The allocated slot, null if we can't find a match
 		 */
 		AllocatedSlot get(final AllocationID allocationID) {
-			return allocatedSlotsById.getKeyA(allocationID);
+			return allocatedSlotsById.getByKeyA(allocationID);
 		}
 
 		AllocatedSlot get(final SlotRequestId slotRequestId) {
-			return allocatedSlotsById.getKeyB(slotRequestId);
+			return allocatedSlotsById.getByKeyB(slotRequestId);
 		}
 
 		/**

flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/slotpool/DualKeyLinkedMapTest.java
@@ -68,9 +68,9 @@ public class DualKeyLinkedMapTest extends TestLogger {
 		map.put(1, 1, "foobar");
 		map.put(1, 2, secondValue);
 
-		assertThat(map.getKeyB(1), nullValue());
-		assertThat(map.getKeyA(1), is(secondValue));
-		assertThat(map.getKeyB(2), is(secondValue));
+		assertThat(map.getByKeyB(1), nullValue());
+		assertThat(map.getByKeyA(1), is(secondValue));
+		assertThat(map.getByKeyB(2), is(secondValue));
 	}
 
 	@Test
@@ -81,8 +81,8 @@ public class DualKeyLinkedMapTest extends TestLogger {
 		map.put(1, 1, "foobar");
 		map.put(2, 1, secondValue);
 
-		assertThat(map.getKeyA(1), nullValue());
-		assertThat(map.getKeyB(1), is(secondValue));
-		assertThat(map.getKeyA(2), is(secondValue));
+		assertThat(map.getByKeyA(1), nullValue());
+		assertThat(map.getByKeyB(1), is(secondValue));
+		assertThat(map.getByKeyA(2), is(secondValue));
 	}
 }

lin9long/myflink

flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/SlotPoolImpl.java
@@ -356,7 +356,7 @@ public class SlotPoolImpl implements SlotPool {
 	}
 
 	private void slotRequestToResourceManagerFailed(SlotRequestId slotRequestID, Throwable failure) {
-		final PendingRequest request = pendingRequests.getKeyA(slotRequestID);
+		final PendingRequest request = pendingRequests.getValueByKeyA(slotRequestID);
 		if (request != null) {
 			if (isBatchRequestAndFailureCanBeIgnored(request, failure)) {
 				log.debug("Ignoring failed request to the resource manager for a batch slot request.");
@@ -694,7 +694,7 @@ public class SlotPoolImpl implements SlotPool {
 
 		componentMainThreadExecutor.assertRunningInMainThread();
 
-		final PendingRequest pendingRequest = pendingRequests.getKeyB(allocationID);
+		final PendingRequest pendingRequest = pendingRequests.getValueByKeyB(allocationID);
 		if (pendingRequest != null) {
 			if (isBatchRequestAndFailureCanBeIgnored(pendingRequest, cause)) {
 				log.debug("Ignoring allocation failure for batch slot request {}.", pendingRequest.getSlotRequestId());
@@ -1050,11 +1050,11 @@ public class SlotPoolImpl implements SlotPool {
 		 * @return The allocated slot, null if we can't find a match
 		 */
 		AllocatedSlot get(final AllocationID allocationID) {
-			return allocatedSlotsById.getKeyA(allocationID);
+			return allocatedSlotsById.getValueByKeyA(allocationID);
 		}
 
 		AllocatedSlot get(final SlotRequestId slotRequestId) {
-			return allocatedSlotsById.getKeyB(slotRequestId);
+			return allocatedSlotsById.getValueByKeyB(slotRequestId);
 		}
 
 		/**

flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/slotpool/DualKeyLinkedMapTest.java
@@ -68,9 +68,9 @@ public class DualKeyLinkedMapTest extends TestLogger {
 		map.put(1, 1, "foobar");
 		map.put(1, 2, secondValue);
 
-		assertThat(map.getKeyB(1), nullValue());
-		assertThat(map.getKeyA(1), is(secondValue));
-		assertThat(map.getKeyB(2), is(secondValue));
+		assertThat(map.getValueByKeyB(1), nullValue());
+		assertThat(map.getValueByKeyA(1), is(secondValue));
+		assertThat(map.getValueByKeyB(2), is(secondValue));
 	}
 
 	@Test
@@ -81,8 +81,8 @@ public class DualKeyLinkedMapTest extends TestLogger {
 		map.put(1, 1, "foobar");
 		map.put(2, 1, secondValue);
 
-		assertThat(map.getKeyA(1), nullValue());
-		assertThat(map.getKeyB(1), is(secondValue));
-		assertThat(map.getKeyA(2), is(secondValue));
+		assertThat(map.getValueByKeyA(1), nullValue());
+		assertThat(map.getValueByKeyB(1), is(secondValue));
+		assertThat(map.getValueByKeyA(2), is(secondValue));
 	}
 }