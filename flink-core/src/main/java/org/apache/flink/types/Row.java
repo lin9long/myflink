apache/flink

[FLINK-16998][core] Add a changeflag to Row
Partial commit for supporting a changeflag without backwards compatibility.

This closes #12103.

flink-core/src/main/java/org/apache/flink/types/Row.java
@@ -18,68 +18,118 @@
 package org.apache.flink.types;
 
 import org.apache.flink.annotation.PublicEvolving;
-import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.util.Preconditions;
 import org.apache.flink.util.StringUtils;
+
+import javax.annotation.Nullable;
 
 import java.io.Serializable;
 import java.util.Arrays;
 
 /**
- * A Row can have arbitrary number of fields and contain a set of fields, which may all be
- * different types. The fields in Row can be null. Due to Row is not strongly typed, Flink's
- * type extraction mechanism can't extract correct field types. So that users should manually
- * tell Flink the type information via creating a {@link RowTypeInfo}.
+ * A row is a fixed-length, null-aware composite type for storing multiple values in a deterministic
+ * field order. Every field can be null regardless of the field's type. The type of row fields cannot
+ * be automatically inferred; therefore, it is required to provide type information whenever a row is
+ * produced.
  *
- * <p>
- * The fields in the Row can be accessed by position (zero-based) {@link #getField(int)}. And can
- * set fields by {@link #setField(int, Object)}.
- * <p>
- * Row is in principle serializable. However, it may contain non-serializable fields,
- * in which case serialization will fail.
+ * <p>The main purpose of rows is to bridge between Flink's table and SQL ecosystem and other APIs. Therefore,
+ * a row does not only consist of a schema part (containing the fields) but also attaches a {@link RowKind}
+ * for encoding a change in a changelog. Thus, a row can be considered as an entry in changelog. For example,
+ * in regular batch scenarios, a changelog would consist of a bounded stream of {@link RowKind#INSERT} rows.
  *
+ * <p>The fields of a row can be accessed by position (zero-based) using {@link #getField(int)} and
+ * {@link #setField(int, Object)}. The row kind is kept separate from the fields and can be accessed
+ * by using {@link #getKind()} and {@link #setKind(RowKind)}.
+ *
+ * <p>A row instance is in principle {@link Serializable}. However, it may contain non-serializable fields
+ * in which case serialization will fail if the row is not serialized with Flink's serialization stack.
  */
 @PublicEvolving
-public class Row implements Serializable{
+public final class Row implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
+
+	/** The kind of change a row describes in a changelog. */
+	private RowKind kind;
 
 	/** The array to store actual values. */
 	private final Object[] fields;
 
 	/**
-	 * Create a new Row instance.
-	 * @param arity The number of fields in the Row
+	 * Create a new row instance.
+	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
+	 *
+	 * @param kind kind of change a row describes in a changelog
+	 * @param arity The number of fields in the row.
 	 */
-	public Row(int arity) {
+	public Row(RowKind kind, int arity) {
+		this.kind = Preconditions.checkNotNull(kind, "Row kind must not be null.");
 		this.fields = new Object[arity];
 	}
 
 	/**
-	 * Get the number of fields in the Row.
-	 * @return The number of fields in the Row.
+	 * Create a new row instance.
+	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
+	 *
+	 * @param arity The number of fields in the row.
+	 */
+	public Row(int arity) {
+		this(RowKind.INSERT, arity);
+	}
+
+	/**
+	 * Returns the kind of change that this row describes in a changelog.
+	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
+	 *
+	 * @see RowKind
+	 */
+	public RowKind getKind() {
+		return kind;
+	}
+
+	/**
+	 * Sets the kind of change that this row describes in a changelog.
+	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
+	 *
+	 * @see RowKind
+	 */
+	public void setKind(RowKind kind) {
+		Preconditions.checkNotNull(kind, "Row kind must not be null.");
+		this.kind = kind;
+	}
+
+	/**
+	 * Returns the number of fields in the row.
+	 *
+	 * <p>Note: The row kind is kept separate from the fields and is not included in this number.
+	 *
+	 * @return The number of fields in the row.
 	 */
 	public int getArity() {
 		return fields.length;
 	}
 
 	/**
-	 * Gets the field at the specified position.
+	 * Returns the field's content at the specified position.
+	 *
 	 * @param pos The position of the field, 0-based.
-	 * @return The field at the specified position.
-	 * @throws IndexOutOfBoundsException Thrown, if the position is negative, or equal to, or larger than the number of fields.
+	 * @return The field's content at the specified position.
 	 */
-	public Object getField(int pos) {
+	public @Nullable Object getField(int pos) {
 		return fields[pos];
 	}
 
 	/**
-	 * Sets the field at the specified position.
+	 * Sets the field's content at the specified position.
 	 *
 	 * @param pos The position of the field, 0-based.
 	 * @param value The value to be assigned to the field at the specified position.
-	 * @throws IndexOutOfBoundsException Thrown, if the position is negative, or equal to, or larger than the number of fields.
 	 */
-	public void setField(int pos, Object value) {
+	public void setField(int pos, @Nullable Object value) {
 		fields[pos] = value;
 	}
 
@@ -103,25 +153,29 @@ public class Row implements Serializable{
 		if (o == null || getClass() != o.getClass()) {
 			return false;
 		}
-
 		Row row = (Row) o;
-
-		return Arrays.deepEquals(fields, row.fields);
+		return kind == row.kind &&
+			Arrays.equals(fields, row.fields);
 	}
 
 	@Override
 	public int hashCode() {
-		return Arrays.deepHashCode(fields);
+		int result = kind.toByteValue(); // for stable hash across JVM instances
+		result = 31 * result + Arrays.hashCode(fields);
+		return result;
 	}
 
+	// --------------------------------------------------------------------------------------------
+	// Utility methods
+	// --------------------------------------------------------------------------------------------
+
 	/**
-	 * Creates a new Row and assigns the given values to the Row's fields.
+	 * Creates a new row and assigns the given values to the row's fields.
 	 * This is more convenient than using the constructor.
 	 *
 	 * <p>For example:
-	 *
 	 * <pre>
-	 *     Row.of("hello", true, 1L);}
+	 *     Row.of("hello", true, 1L);
 	 * </pre>
 	 * instead of
 	 * <pre>
@@ -131,6 +185,7 @@ public class Row implements Serializable{
 	 *     row.setField(2, 1L);
 	 * </pre>
 	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
 	 */
 	public static Row of(Object... values) {
 		Row row = new Row(values.length);
@@ -141,27 +196,50 @@ public class Row implements Serializable{
 	}
 
 	/**
-	 * Creates a new Row which copied from another row.
-	 * This method does not perform a deep copy.
+	 * Creates a new row with given kind and assigns the given values to the row's fields.
+	 * This is more convenient than using the constructor.
 	 *
-	 * @param row The row being copied.
-	 * @return The cloned new Row
+	 * <p>For example:
+	 * <pre>
+	 *     Row.ofKind(RowKind.INSERT, "hello", true, 1L);
+	 * </pre>
+	 * instead of
+	 * <pre>
+	 *     Row row = new Row(3);
+	 *     row.setKind(RowKind.INSERT);
+	 *     row.setField(0, "hello");
+	 *     row.setField(1, true);
+	 *     row.setField(2, 1L);
+	 * </pre>
+	 */
+	public static Row ofKind(RowKind kind, Object... values) {
+		Row row = new Row(kind, values.length);
+		for (int i = 0; i < values.length; i++) {
+			row.setField(i, values[i]);
+		}
+		return row;
+	}
+
+	/**
+	 * Creates a new row which is copied from another row (including its {@link RowKind}).
+	 *
+	 * <p>This method does not perform a deep copy.
 	 */
 	public static Row copy(Row row) {
-		final Row newRow = new Row(row.fields.length);
+		final Row newRow = new Row(row.kind, row.fields.length);
 		System.arraycopy(row.fields, 0, newRow.fields, 0, row.fields.length);
 		return newRow;
 	}
 
 	/**
-	 * Creates a new Row with projected fields from another row.
-	 * This method does not perform a deep copy.
+	 * Creates a new row with projected fields and identical {@link RowKind} from another row.
 	 *
-	 * @param fields fields to be projected
-	 * @return the new projected Row
+	 * <p>This method does not perform a deep copy.
+	 *
+	 * @param fields field indices to be projected
 	 */
 	public static Row project(Row row, int[] fields) {
-		final Row newRow = new Row(fields.length);
+		final Row newRow = new Row(row.kind, fields.length);
 		for (int i = 0; i < fields.length; i++) {
 			newRow.fields[i] = row.fields[fields[i]];
 		}
@@ -169,12 +247,11 @@ public class Row implements Serializable{
 	}
 
 	/**
-	 * Creates a new Row which fields are copied from the other rows.
-	 * This method does not perform a deep copy.
+	 * Creates a new row with fields that are copied from the other rows and appended to the resulting
+	 * row in the given order. The {@link RowKind} of the first row determines the {@link RowKind} of
+	 * the result.
 	 *
-	 * @param first The first row being copied.
-	 * @param remainings The other rows being copied.
-	 * @return the joined new Row
+	 * <p>This method does not perform a deep copy.
 	 */
 	public static Row join(Row first, Row... remainings) {
 		int newLength = first.fields.length;
@@ -182,7 +259,7 @@ public class Row implements Serializable{
 			newLength += remaining.fields.length;
 		}
 
-		final Row joinedRow = new Row(newLength);
+		final Row joinedRow = new Row(first.kind, newLength);
 		int index = 0;
 
 		// copy the first row

lin9long/myflink

flink-core/src/main/java/org/apache/flink/types/Row.java
@@ -18,68 +18,118 @@
 package org.apache.flink.types;
 
 import org.apache.flink.annotation.PublicEvolving;
-import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.util.Preconditions;
 import org.apache.flink.util.StringUtils;
+
+import javax.annotation.Nullable;
 
 import java.io.Serializable;
 import java.util.Arrays;
 
 /**
- * A Row can have arbitrary number of fields and contain a set of fields, which may all be
- * different types. The fields in Row can be null. Due to Row is not strongly typed, Flink's
- * type extraction mechanism can't extract correct field types. So that users should manually
- * tell Flink the type information via creating a {@link RowTypeInfo}.
+ * A row is a fixed-length, null-aware composite type for storing multiple values in a deterministic
+ * field order. Every field can be null regardless of the field's type. The type of row fields cannot
+ * be automatically inferred; therefore, it is required to provide type information whenever a row is
+ * produced.
  *
- * <p>
- * The fields in the Row can be accessed by position (zero-based) {@link #getField(int)}. And can
- * set fields by {@link #setField(int, Object)}.
- * <p>
- * Row is in principle serializable. However, it may contain non-serializable fields,
- * in which case serialization will fail.
+ * <p>The main purpose of rows is to bridge between Flink's Table and SQL ecosystem and other APIs. Therefore,
+ * a row does not only consist of a schema part (containing the fields) but also attaches a {@link RowKind}
+ * for encoding a change in a changelog. Thus, a row can be considered as an entry in a changelog. For example,
+ * in regular batch scenarios, a changelog would consist of a bounded stream of {@link RowKind#INSERT} rows.
  *
+ * <p>The fields of a row can be accessed by position (zero-based) using {@link #getField(int)} and
+ * {@link #setField(int, Object)}. The row kind is kept separate from the fields and can be accessed
+ * by using {@link #getKind()} and {@link #setKind(RowKind)}.
+ *
+ * <p>A row instance is in principle {@link Serializable}. However, it may contain non-serializable fields
+ * in which case serialization will fail if the row is not serialized with Flink's serialization stack.
  */
 @PublicEvolving
-public class Row implements Serializable{
+public final class Row implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
+
+	/** The kind of change a row describes in a changelog. */
+	private RowKind kind;
 
 	/** The array to store actual values. */
 	private final Object[] fields;
 
 	/**
-	 * Create a new Row instance.
-	 * @param arity The number of fields in the Row
+	 * Create a new row instance.
+	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
+	 *
+	 * @param kind kind of change a row describes in a changelog
+	 * @param arity The number of fields in the row.
 	 */
-	public Row(int arity) {
+	public Row(RowKind kind, int arity) {
+		this.kind = Preconditions.checkNotNull(kind, "Row kind must not be null.");
 		this.fields = new Object[arity];
 	}
 
 	/**
-	 * Get the number of fields in the Row.
-	 * @return The number of fields in the Row.
+	 * Create a new row instance.
+	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
+	 *
+	 * @param arity The number of fields in the row.
+	 */
+	public Row(int arity) {
+		this(RowKind.INSERT, arity);
+	}
+
+	/**
+	 * Returns the kind of change that this row describes in a changelog.
+	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
+	 *
+	 * @see RowKind
+	 */
+	public RowKind getKind() {
+		return kind;
+	}
+
+	/**
+	 * Sets the kind of change that this row describes in a changelog.
+	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
+	 *
+	 * @see RowKind
+	 */
+	public void setKind(RowKind kind) {
+		Preconditions.checkNotNull(kind, "Row kind must not be null.");
+		this.kind = kind;
+	}
+
+	/**
+	 * Returns the number of fields in the row.
+	 *
+	 * <p>Note: The row kind is kept separate from the fields and is not included in this number.
+	 *
+	 * @return The number of fields in the row.
 	 */
 	public int getArity() {
 		return fields.length;
 	}
 
 	/**
-	 * Gets the field at the specified position.
+	 * Returns the field's content at the specified position.
+	 *
 	 * @param pos The position of the field, 0-based.
-	 * @return The field at the specified position.
-	 * @throws IndexOutOfBoundsException Thrown, if the position is negative, or equal to, or larger than the number of fields.
+	 * @return The field's content at the specified position.
 	 */
-	public Object getField(int pos) {
+	public @Nullable Object getField(int pos) {
 		return fields[pos];
 	}
 
 	/**
-	 * Sets the field at the specified position.
+	 * Sets the field's content at the specified position.
 	 *
 	 * @param pos The position of the field, 0-based.
 	 * @param value The value to be assigned to the field at the specified position.
-	 * @throws IndexOutOfBoundsException Thrown, if the position is negative, or equal to, or larger than the number of fields.
 	 */
-	public void setField(int pos, Object value) {
+	public void setField(int pos, @Nullable Object value) {
 		fields[pos] = value;
 	}
 
@@ -103,25 +153,29 @@ public class Row implements Serializable{
 		if (o == null || getClass() != o.getClass()) {
 			return false;
 		}
-
 		Row row = (Row) o;
-
-		return Arrays.deepEquals(fields, row.fields);
+		return kind == row.kind &&
+			Arrays.deepEquals(fields, row.fields);
 	}
 
 	@Override
 	public int hashCode() {
-		return Arrays.deepHashCode(fields);
+		int result = kind.toByteValue(); // for stable hash across JVM instances
+		result = 31 * result + Arrays.deepHashCode(fields);
+		return result;
 	}
 
+	// --------------------------------------------------------------------------------------------
+	// Utility methods
+	// --------------------------------------------------------------------------------------------
+
 	/**
-	 * Creates a new Row and assigns the given values to the Row's fields.
+	 * Creates a new row and assigns the given values to the row's fields.
 	 * This is more convenient than using the constructor.
 	 *
 	 * <p>For example:
-	 *
 	 * <pre>
-	 *     Row.of("hello", true, 1L);}
+	 *     Row.of("hello", true, 1L);
 	 * </pre>
 	 * instead of
 	 * <pre>
@@ -131,6 +185,7 @@ public class Row implements Serializable{
 	 *     row.setField(2, 1L);
 	 * </pre>
 	 *
+	 * <p>By default, a row describes an {@link RowKind#INSERT} change.
 	 */
 	public static Row of(Object... values) {
 		Row row = new Row(values.length);
@@ -141,27 +196,50 @@ public class Row implements Serializable{
 	}
 
 	/**
-	 * Creates a new Row which copied from another row.
-	 * This method does not perform a deep copy.
+	 * Creates a new row with given kind and assigns the given values to the row's fields.
+	 * This is more convenient than using the constructor.
 	 *
-	 * @param row The row being copied.
-	 * @return The cloned new Row
+	 * <p>For example:
+	 * <pre>
+	 *     Row.ofKind(RowKind.INSERT, "hello", true, 1L);
+	 * </pre>
+	 * instead of
+	 * <pre>
+	 *     Row row = new Row(3);
+	 *     row.setKind(RowKind.INSERT);
+	 *     row.setField(0, "hello");
+	 *     row.setField(1, true);
+	 *     row.setField(2, 1L);
+	 * </pre>
+	 */
+	public static Row ofKind(RowKind kind, Object... values) {
+		Row row = new Row(kind, values.length);
+		for (int i = 0; i < values.length; i++) {
+			row.setField(i, values[i]);
+		}
+		return row;
+	}
+
+	/**
+	 * Creates a new row which is copied from another row (including its {@link RowKind}).
+	 *
+	 * <p>This method does not perform a deep copy.
 	 */
 	public static Row copy(Row row) {
-		final Row newRow = new Row(row.fields.length);
+		final Row newRow = new Row(row.kind, row.fields.length);
 		System.arraycopy(row.fields, 0, newRow.fields, 0, row.fields.length);
 		return newRow;
 	}
 
 	/**
-	 * Creates a new Row with projected fields from another row.
-	 * This method does not perform a deep copy.
+	 * Creates a new row with projected fields and identical {@link RowKind} from another row.
 	 *
-	 * @param fields fields to be projected
-	 * @return the new projected Row
+	 * <p>This method does not perform a deep copy.
+	 *
+	 * @param fields field indices to be projected
 	 */
 	public static Row project(Row row, int[] fields) {
-		final Row newRow = new Row(fields.length);
+		final Row newRow = new Row(row.kind, fields.length);
 		for (int i = 0; i < fields.length; i++) {
 			newRow.fields[i] = row.fields[fields[i]];
 		}
@@ -169,12 +247,11 @@ public class Row implements Serializable{
 	}
 
 	/**
-	 * Creates a new Row which fields are copied from the other rows.
-	 * This method does not perform a deep copy.
+	 * Creates a new row with fields that are copied from the other rows and appended to the resulting
+	 * row in the given order. The {@link RowKind} of the first row determines the {@link RowKind} of
+	 * the result.
 	 *
-	 * @param first The first row being copied.
-	 * @param remainings The other rows being copied.
-	 * @return the joined new Row
+	 * <p>This method does not perform a deep copy.
 	 */
 	public static Row join(Row first, Row... remainings) {
 		int newLength = first.fields.length;
@@ -182,7 +259,7 @@ public class Row implements Serializable{
 			newLength += remaining.fields.length;
 		}
 
-		final Row joinedRow = new Row(newLength);
+		final Row joinedRow = new Row(first.kind, newLength);
 		int index = 0;
 
 		// copy the first row