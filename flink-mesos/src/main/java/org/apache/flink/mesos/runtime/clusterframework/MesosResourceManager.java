xintongsong/flink

[hotfix][runtime] Refactor resource manager termination handling.

flink-mesos/src/main/java/org/apache/flink/mesos/runtime/clusterframework/MesosResourceManager.java
@@ -394,8 +394,8 @@ public class MesosResourceManager extends ResourceManager<RegisteredMesosWorkerN
 	}
 
 	@Override
-	public CompletableFuture<Void> onStop() {
-		return stopSupportingActorsAsync().thenCompose((ignored) -> super.onStop());
+	public void terminate() throws Throwable {
+		stopSupportingActorsAsync().get();
 	}
 
 	@Override

flink-runtime/src/main/java/org/apache/flink/runtime/resourcemanager/ResourceManager.java
@@ -242,7 +242,7 @@ public abstract class ResourceManager<WorkerType extends ResourceIDRetrievable>
 	}
 
 	@Override
-	public CompletableFuture<Void> onStop() {
+	public final CompletableFuture<Void> onStop() {
 		try {
 			stopResourceManagerServices();
 		} catch (Exception exception) {
@@ -255,6 +255,12 @@ public abstract class ResourceManager<WorkerType extends ResourceIDRetrievable>
 
 	private void stopResourceManagerServices() throws Exception {
 		Exception exception = null;
+
+		try {
+			terminate();
+		} catch (Throwable t) {
+			exception = new ResourceManagerException("Error while shutting down resource manager", t);
+		}
 
 		stopHeartbeatServices();
 
@@ -1063,6 +1069,13 @@ public abstract class ResourceManager<WorkerType extends ResourceIDRetrievable>
 	 * @throws ResourceManagerException which occurs during initialization and causes the resource manager to fail.
 	 */
 	protected abstract void initialize() throws ResourceManagerException;
+
+	/**
+	 * Terminates the framework specific components.
+	 *
+	 * @throws Throwable with occurs during termination.
+	 */
+	protected abstract void terminate() throws Throwable;
 
 	/**
 	 * This method can be overridden to add a (non-blocking) initialization routine to the

lin9long/myflink

flink-mesos/src/main/java/org/apache/flink/mesos/runtime/clusterframework/MesosResourceManager.java
@@ -394,8 +394,8 @@ public class MesosResourceManager extends ResourceManager<RegisteredMesosWorkerN
 	}
 
 	@Override
-	public CompletableFuture<Void> onStop() {
-		return stopSupportingActorsAsync().thenCompose((ignored) -> super.onStop());
+	public void terminate() throws Exception {
+		stopSupportingActorsAsync().get();
 	}
 
 	@Override

flink-runtime/src/main/java/org/apache/flink/runtime/resourcemanager/ResourceManager.java
@@ -242,7 +242,7 @@ public abstract class ResourceManager<WorkerType extends ResourceIDRetrievable>
 	}
 
 	@Override
-	public CompletableFuture<Void> onStop() {
+	public final CompletableFuture<Void> onStop() {
 		try {
 			stopResourceManagerServices();
 		} catch (Exception exception) {
@@ -255,6 +255,12 @@ public abstract class ResourceManager<WorkerType extends ResourceIDRetrievable>
 
 	private void stopResourceManagerServices() throws Exception {
 		Exception exception = null;
+
+		try {
+			terminate();
+		} catch (Exception e) {
+			exception = new ResourceManagerException("Error while shutting down resource manager", e);
+		}
 
 		stopHeartbeatServices();
 
@@ -1063,6 +1069,13 @@ public abstract class ResourceManager<WorkerType extends ResourceIDRetrievable>
 	 * @throws ResourceManagerException which occurs during initialization and causes the resource manager to fail.
 	 */
 	protected abstract void initialize() throws ResourceManagerException;
+
+	/**
+	 * Terminates the framework specific components.
+	 *
+	 * @throws Exception which occurs during termination.
+	 */
+	protected abstract void terminate() throws Exception;
 
 	/**
 	 * This method can be overridden to add a (non-blocking) initialization routine to the